2c962de85d7cf8a8049f8c96bd7045ce0ad23ac8
Ash Corah <ash.corah@chaser.io>
2021-03-07 13:54:08 +0000

    add instagram api

--- src/services/getInstagramAccount.js
+++ src/services/getInstagramAccount.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const token =
-  "IGQVJYcWRIRVJjM1Q3dDU4OTY4LThJbWlvYlNsNTNyaHc5dXIzNmdaVEtzNDhFTGk5RVlUdmVDTVZAET2k2di11d0w5X1ZAaS0x1MUJmaXUxcTliV1BfbjloLW9pNHV0ZAUJFS2VxZAmlSQ0FVaG1ldGVxcQZDZDv";
+const token = "";
 
 async function getInstagramAccount(fields) {
   const { data } = await axios.get(
