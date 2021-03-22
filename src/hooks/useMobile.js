638c26a9f4088bed94b91890130ee3fb798a01a3
Ash Corah <ashcorah@gmail.com>
2021-03-23 00:56:49 +0100

    add missing bracket

--- src/hooks/useMobile.js
+++ src/hooks/useMobile.js
@@ -1,7 +1,7 @@
 import { useMediaQuery } from "@chakra-ui/react";
 
 function useMobile() {
-  const [isMobile] = useMediaQuery("(max-width: 850px");
+  const [isMobile] = useMediaQuery("(max-width: 850px)");
   return isMobile;
 }
 
