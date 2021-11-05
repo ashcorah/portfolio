83f922e7d9bac2840b6734da0751d61b6b2742ea
Ash Corah <ashcorah@gmail.com>
2021-11-05 01:36:28 +0100

    fix token

--- src/services/getInstagramAccount.js
+++ src/services/getInstagramAccount.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const token =
-  "IGQVJVZAjFYUXl4bkQ1cU5qWmpuN3JxNFFOSVowYVNRWnh0Vm04UzRDeTdSel9mQzdIM3JiblJHUElHQXg1SDRWektzQ1YwRkw4ZA0lYM1ZAXRUQxWURIQnFNT0ZAUbG16MndZAVHZAkdmpheFZAJZAVExRXk4RAZDZD";
+  "IGQVJVZAnU4WUxRMm9rYjZAVTVJvQmpueTRzM0NBYkU4RjJLcDdkQ28wZA1hkUnVWZAkZAPQ1pIN21nTVg2WDlPU051ZAXljb1hrR25rTWRzcUFXbkwtbVVwRmg3STdqMWVUS0JuN3o0WXlQaGRsVUxGeXk0dAZDZD";
 
 async function getInstagramAccount(fields) {
   console.log("refetching");
