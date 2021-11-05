0dd29f03cfa81c16ff01e8a44112dc473c6cd7ae
Ash Corah <ashcorah@gmail.com>
2021-11-05 22:30:07 +0100

    remove log

--- src/services/getInstagramAccount.js
+++ src/services/getInstagramAccount.js
@@ -4,7 +4,6 @@ const token =
   process.env.INSTAGRAM_TOKEN;
 
 async function getInstagramAccount(fields) {
-  console.log("refetching");
   const { data } = await axios.get(
     `https://graph.instagram.com/me/media?fields=${fields}&access_token=id,username&access_token=${token}`
   );
