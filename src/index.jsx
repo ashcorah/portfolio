c84eda2c8ea7b1b322da206959cda5c40725475e
Ash Corah <ashcorah@gmail.com>
2025-01-26 14:54:14 +0000

    fix package-lock

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { ThemeProvider } from "./components/ThemeSelector/themeContext";
+import { ThemeProvider } from "./components/ThemeSelector/themeContext.js";
 
 ReactDOM.render(
   <ThemeProvider>
